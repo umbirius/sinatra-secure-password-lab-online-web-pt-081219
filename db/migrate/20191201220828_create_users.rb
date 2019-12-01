2374e800829f61515b50241deeeddb116733747a
Umberto Palazzo <upalazzo93@gmail.com>
2019-12-01 22:11:11 +0000

    Automatically backed up by Learn

--- db/migrate/20191201220828_create_users.rb
+++ db/migrate/20191201220828_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.text "username"
-      t.text "password"
+      t.text :username
+      t.text :password
       
   end
 end
