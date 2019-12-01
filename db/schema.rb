a95f209e8c9d93a71096bffc55b9626eecbfff11
Umberto Palazzo <upalazzo93@gmail.com>
2019-12-01 22:12:07 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191201220828) do
-
-  create_table "users", force: :cascade do |t|
-    t.text "username"
-    t.text "password"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
